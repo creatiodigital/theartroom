d5bc8fd2a18540c54a8c2b41da1aa54bf9f8d621
creatiodigital <contact@pixelatelier.com>
2026-01-12 18:11:18 +0100

    new logo

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import Link from 'next/link'
-import { Text } from '@/components/ui/Typography'
+
+import Logo from '@/icons/logo.svg'
 
 import styles from './Footer.module.scss'
 
@@ -9,7 +10,9 @@ export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerInner}>
-        <Text as="h3" className={styles.brand}>The Foundation</Text>
+        <Link href="/" className={styles.logoLink}>
+          <Logo className={styles.logo} />
+        </Link>
         <nav className={styles.links}>
           <Link href="/terms-and-conditions">Terms and Conditions</Link>
           <Link href="/privacy-policy">Privacy Policy</Link>
